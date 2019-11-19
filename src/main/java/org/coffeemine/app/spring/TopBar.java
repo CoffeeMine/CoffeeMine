a366f0cba3b9ed6d4216312e5581740badba63fd
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-11-19 13:44:27 +0100

    TopBar: Use Tabs instead of RouterLink.

--- src/main/java/org/coffeemine/app/spring/TopBar.java
+++ src/main/java/org/coffeemine/app/spring/TopBar.java
@@ -1,14 +1,39 @@
 package org.coffeemine.app.spring;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteData;
+import com.vaadin.flow.router.Router;
 
 public class TopBar extends HorizontalLayout {
-    public TopBar(){
-        final var router = UI.getCurrent().getRouter();
-        final var routes = router.getRegistry().getRegisteredRoutes();
+    static int tabIndex = 0;
 
-        routes.forEach(r -> add(new RouterLink(r.getNavigationTarget().getSimpleName(), r.getNavigationTarget())));
+    public TopBar() {
+        Router router = UI.getCurrent().getRouter();
+        List<RouteData> routes = router.getRegistry().getRegisteredRoutes();
+
+        Map<Tab, RouteData> tabRoute = new HashMap<>();
+        Tabs routingTabs = new Tabs();
+
+        routes.forEach(r -> {
+            Tab tab = new Tab(r.getNavigationTarget().getSimpleName());
+            routingTabs.add(tab);
+            tabRoute.put(tab, r);
+        });
+
+        routingTabs.addSelectedChangeListener(e -> {
+            tabIndex = routingTabs.getSelectedIndex();
+            routingTabs.getUI().ifPresent(ui -> ui.navigate(tabRoute.get(routingTabs.getSelectedTab()).getNavigationTarget()));
+        });
+
+        routingTabs.setSelectedIndex(tabIndex);
+        add(routingTabs); 
     }
 }
