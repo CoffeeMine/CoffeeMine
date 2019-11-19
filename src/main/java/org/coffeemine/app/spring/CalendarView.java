ebe58fef7975321f4e6664a2fe6a13b0954be192
chenr <chenr@student.chalmers.se>
2019-11-19 15:31:09 +0100

    Update calendar component
    For teammate works

---
+++ src/main/java/org/coffeemine/app/spring/CalendarView.java
@@ -0,0 +1,9 @@
+package org.coffeemine.app.spring;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class CalendarView extends VerticalLayout {
+    public CalendarView(){
+
+    }
+}
