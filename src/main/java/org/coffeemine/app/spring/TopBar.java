abceef65c21d5210475736a1f7900527bd42817b
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-11-24 11:30:51 +0100

    Move tab routing generation logic to a static method. In the future we might want to "object oriented"-ify this
    Signed-off-by: Ruthger Dijt <ruthgerdijt@gmail.com>

--- src/main/java/org/coffeemine/app/spring/TopBar.java
+++ src/main/java/org/coffeemine/app/spring/TopBar.java
@@ -1,18 +1,13 @@
 package org.coffeemine.app.spring;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.router.RouteData;
-import com.vaadin.flow.router.Router;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
+
+import com.vaadin.flow.router.Route;
 
 public class TopBar extends HorizontalLayout {
     static int tabIndex = 0;
@@ -26,27 +21,10 @@ public class TopBar extends HorizontalLayout {
         HorizontalLayout miscLayout = new HorizontalLayout();
 
         this.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
-        tabLayout.setWidthFull();
 
-        Router router = UI.getCurrent().getRouter();
-        List<RouteData> routes = router.getRegistry().getRegisteredRoutes();
+        tabLayout.setSizeFull();
+        tabLayout.add(RoutingTabs.getTabs());
 
-        Map<Tab, RouteData> tabRoute = new HashMap<>();
-        Tabs routingTabs = new Tabs();
-        
-        routes.forEach(r -> {
-            Tab tab = new Tab(r.getNavigationTarget().getSimpleName());
-            routingTabs.add(tab);
-            tabRoute.put(tab, r);
-        });
-
-        routingTabs.addSelectedChangeListener(e -> {
-            tabIndex = routingTabs.getSelectedIndex();
-            routingTabs.getUI().ifPresent(ui -> ui.navigate(tabRoute.get(routingTabs.getSelectedTab()).getNavigationTarget()));
-        });
-
-        routingTabs.setSelectedIndex(tabIndex);
-        tabLayout.add(routingTabs);
 
         Button AccountButton = new Button("Account");
 
