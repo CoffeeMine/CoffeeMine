a36a7ad0dfa53548a59ea190a1793b87467decfd
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-12-08 15:11:59 +0100

    Basic lock out when there are no users loged in (example), For now we have to apply this "security" manually to all views. Anything inheriting from View.class is covered with this commit.
    Signed-off-by: Ruthger Dijt <ruthgerdijt@gmail.com>

--- src/main/java/org/coffeemine/app/spring/View.java
+++ src/main/java/org/coffeemine/app/spring/View.java
@@ -3,8 +3,13 @@ package org.coffeemine.app.spring;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 
-public abstract class View extends AppLayout {
+import org.coffeemine.app.spring.auth.BasicAccessControl;
+import org.coffeemine.app.spring.auth.LoginScreen;
+
+public abstract class View extends AppLayout implements BeforeEnterObserver {
     protected Div content_div = new Div();
 
     protected View(){
@@ -18,5 +23,12 @@ public abstract class View extends AppLayout {
     }
     public void add(Component... components){
         content_div.add(components);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        if (!BasicAccessControl.getInstance().isUserSignedIn()) {
+            event.forwardTo(LoginScreen.class);
+        }
     }
 }
