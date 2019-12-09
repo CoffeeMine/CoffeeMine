7700a37c2ede0e183ae76002c31afd5e524e38da
BhavyaShukla <bhavyashukla2002@gmail.com>
2019-12-09 18:34:47 +0100

    Changed the statistics page
    Removed some spans as they weren't needed, added some graphs so that it looks nice

--- src/main/java/org/coffeemine/app/spring/Statistics.java
+++ src/main/java/org/coffeemine/app/spring/Statistics.java
@@ -1,6 +1,7 @@
 package org.coffeemine.app.spring;
 
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Span;
@@ -12,59 +13,49 @@ public class Statistics extends View {
     public Statistics() {
         FormLayout layout = new FormLayout();
         H1 header = new H1("Statistics");
-        H3 eva = new H3("Earned Value Analysis");
+        H3 subtitle = new H3("Earned Value Analysis");
 
         this.add(header);
-        this.add(eva);
+        this.add(subtitle);
         this.add(layout);
 
-        Span ipb = new Span("");
-        layout.addFormItem(ipb, "Initially Planned Budget");
-
-        Span av = new Span("");
-        layout.addFormItem(av, "Actual Value");
-
-        Span pv = new Span("");
-        layout.addFormItem(pv, "Planned Value");
-
-        Span cv = new Span("");
-        layout.addFormItem(cv, "Cost Variance");
-
-        Span sv = new Span("");
-        layout.addFormItem(sv, "Schedule Variance");
-
-        Span cpi = new Span("");
-        layout.addFormItem(cpi, "Cost Performance Index");
-
-        Span spi = new Span("");
-        layout.addFormItem(spi, "Schedule Performance Index");
-
-        H3 rm = new H3("Risk Matrix");
-        this.add(rm);
-
-        FormLayout matrixLayout = new FormLayout();
-        this.add(matrixLayout);
-
         Span initiallyPlannedBudget = new Span("");
-        matrixLayout.addFormItem(initiallyPlannedBudget, "Initially Planned Budget");
+        layout.addFormItem(initiallyPlannedBudget, "Initially Planned Budget");
 
         Span actualValue = new Span("");
-        matrixLayout.addFormItem(actualValue, "Actual Value");
+        layout.addFormItem(actualValue, "Actual Value");
 
         Span plannedValue = new Span("");
-        matrixLayout.addFormItem(plannedValue, "Planned Value");
+        layout.addFormItem(plannedValue, "Planned Value");
 
         Span costVariance = new Span("");
-        matrixLayout.addFormItem(costVariance, "Cost Variance");
+        layout.addFormItem(costVariance, "Cost Variance");
 
         Span scheduleVariance = new Span("");
-        matrixLayout.addFormItem(scheduleVariance, "Schedule Variance");
+        layout.addFormItem(scheduleVariance, "Schedule Variance");
 
         Span costPerformance = new Span("");
-        matrixLayout.addFormItem(costPerformance, "Cost Performance Index");
+        layout.addFormItem(costPerformance, "Cost Performance Index");
 
         Span schedulePerformance = new Span("");
-        matrixLayout.addFormItem(schedulePerformance, "Schedule Performance Index");
+        layout.addFormItem(schedulePerformance, "Schedule Performance Index");
+
+        Div barChartLayout = new Div();
+        barChartLayout.getStyle().set("margin", "auto");
+        barChartLayout.setMaxWidth("800px");
+        this.add(barChartLayout);
+        BarChart barChart = new BarChart();
+        barChartLayout.add(barChart);
+
+        Div radialChartLayout = new Div();
+        radialChartLayout.getStyle().set("margin", "auto");
+        radialChartLayout.setMaxWidth("400px");
+        this.add(radialChartLayout);
+        RadialBarChart radialBarChart = new RadialBarChart();
+        radialChartLayout.add(radialBarChart);
+
+
+
 
     }
 
