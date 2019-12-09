7eac4ccae278094fe104b89ffa7cc5f435cee319
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-12-09 10:53:37 +0100

    Cleanup
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- src/main/java/org/coffeemine/app/spring/components/JSONUploadSection/JSONUploadSection.java
+++ src/main/java/org/coffeemine/app/spring/components/JSONUploadSection/JSONUploadSection.java
@@ -1,25 +1,37 @@
 package org.coffeemine.app.spring.components.JSONUploadSection;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.component.upload.Upload;
-import com.vaadin.flow.component.upload.receivers.FileBuffer;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.templatemodel.TemplateModel;
+import org.coffeemine.app.spring.db.NitriteDBProvider;
+
+import java.io.IOException;
 
 @Tag("json-upload-section")
 @JsModule("./src/JSONUploadSection.js")
 public class JSONUploadSection extends PolymerTemplate<TemplateModel> {
-    private FileBuffer file_buffer = new FileBuffer();
+    private MemoryBuffer memory_buffer = new MemoryBuffer();
 
     @Id("upload")
     private Upload upload;
+    @Id("import")
+    private Button button;
 
     public JSONUploadSection() {
         super();
-        upload.setReceiver(file_buffer);
-        upload.setAutoUpload(false);
+        upload.setReceiver(memory_buffer);
         upload.setMaxFiles(1);
+        button.addClickListener(e -> {
+            try {
+                NitriteDBProvider.getInstance().importJSONProject(new String(memory_buffer.getInputStream().readAllBytes()));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 }
