56700b0150a18e08cf172e2f8f8561236273fdc8
ZJW-92 <57632939+ZJW-92@users.noreply.github.com>
2019-12-09 14:37:56 +0100

    UserProfile: derive letter icon from full name / formatting

--- src/main/java/org/coffeemine/app/spring/UserProfile.java
+++ src/main/java/org/coffeemine/app/spring/UserProfile.java
@@ -1,4 +1,6 @@
 package org.coffeemine.app.spring;
+
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
@@ -10,6 +12,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
+//bbbb
 @Route(value = "Profile")
 class UserProfile extends VerticalLayout {
     public UserProfile() {
@@ -19,15 +22,17 @@ class UserProfile extends VerticalLayout {
         userprofile.getStyle().set("margin", "auto");
         this.add(userprofile);
         Button back = new Button("back");
+        back.addClickListener(e -> UI.getCurrent().navigate(Overview.class));
         back.addThemeVariants(ButtonVariant.MATERIAL_OUTLINED);
         HorizontalLayout usernameTitle = new HorizontalLayout();
         usernameTitle.getStyle().set("margin", "auto");
-        H2 title = new H2("Account Name");
+        String accountName = "Account Name";
+        H2 title = new H2(accountName);
         title.getStyle().set("padding", "0px");
         title.getStyle().set("margin-top", "auto");
         title.getStyle().set("margin-bottom", "auto");
         // circle capital letter A
-        Span letter = new Span("A");
+        Span letter = new Span(accountName.substring(0, 1));
         letter.addClassName("lettericon");
         usernameTitle.add(letter, title);
         userprofile.add(back);
@@ -47,8 +52,8 @@ class UserProfile extends VerticalLayout {
         states.setWidthFull();
         states.add(new H3("Account states "));
         FormLayout accountStates = new FormLayout();
-        accountStates.addFormItem(new Span("0.0"),"Hourly salary");
-        accountStates.addFormItem(new Span("0.0"),"Total hours worked: ");
+        accountStates.addFormItem(new Span("0.0"), "Hourly salary");
+        accountStates.addFormItem(new Span("0.0"), "Total hours worked: ");
         states.add(accountStates);
         info.add(details, states);
         H3 currentProject = new H3("Current projects");
@@ -56,12 +61,13 @@ class UserProfile extends VerticalLayout {
         save.getStyle().set("margin-left", "auto");
         save.addThemeVariants(ButtonVariant.MATERIAL_OUTLINED);
         HorizontalLayout currentProjects = new HorizontalLayout();
-        H3 lorem = new H3("Lorem Ipsum");
+        String projectName = "Lorem Ipsum";
+        H3 lorem = new H3(projectName);
         lorem.getStyle().set("padding", "0px");
         lorem.getStyle().set("margin-top", "auto");
         lorem.getStyle().set("margin-bottom", "auto");
         // circle capital letter L
-        LetterIcon L = new LetterIcon("L");
+        LetterIcon L = new LetterIcon(projectName.substring(0, 1));
         L.getStyle().set("font-size", "30px");
         currentProjects.add(L, lorem);
         userprofile.add(save);
