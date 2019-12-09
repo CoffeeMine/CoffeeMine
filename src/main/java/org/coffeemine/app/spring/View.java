c62da585f90fc2aa716f1148d3ed6ef8728b4dd8
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-12-09 21:06:03 +0100

    Move login lock to defaulted interface (mixin)

--- src/main/java/org/coffeemine/app/spring/View.java
+++ src/main/java/org/coffeemine/app/spring/View.java
@@ -3,13 +3,8 @@ package org.coffeemine.app.spring;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
 
-import org.coffeemine.app.spring.auth.BasicAccessControl;
-import org.coffeemine.app.spring.auth.LoginScreen;
-
-public abstract class View extends AppLayout implements BeforeEnterObserver {
+public abstract class View extends AppLayout implements ProtectedView {
     protected Div content_div = new Div();
 
     protected View() {
@@ -21,12 +16,5 @@ public abstract class View extends AppLayout implements BeforeEnterObserver {
 
     public void add(Component... components) {
         content_div.add(components);
-    }
-
-    @Override
-    public void beforeEnter(BeforeEnterEvent event) {
-        if (!BasicAccessControl.getInstance().isUserSignedIn()) {
-            event.forwardTo(LoginScreen.class);
-        }
     }
 }
