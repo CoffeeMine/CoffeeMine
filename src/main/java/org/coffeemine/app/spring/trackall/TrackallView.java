dd083aeb7945a649abfb36859745f69db92e2c40
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-12-13 09:54:16 +0100

    Convert Trackall items to TrackItem consumers
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- src/main/java/org/coffeemine/app/spring/trackall/TrackallView.java
+++ src/main/java/org/coffeemine/app/spring/trackall/TrackallView.java
@@ -1,36 +1,47 @@
 package org.coffeemine.app.spring.trackall;
 
 import ch.carnet.kasparscherrer.VerticalScrollLayout;
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
 import org.coffeemine.app.spring.annonations.NavbarRoutable;
+import org.coffeemine.app.spring.data.TrackItem;
 import org.coffeemine.app.spring.view.View;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @Route(value = "Tracker")
 @NavbarRoutable
 public class TrackallView extends View {
-    private static class ListItem extends Div {
-        ListItem() {
-            super();
-            getStyle().set("border-bottom", "1px solid yellow");
-        }
+    private static class ListItem extends Tab {
 
-        ListItem(Component... components) {
-            this();
-            super.add(components);
+        private TrackItem item;
+
+        ListItem(TrackItem item) {
+            super();
+            this.item = item;
+            getStyle().set("border-bottom", "1px solid yellow");
+            final var inner_container = new VerticalLayout(
+                    new Span('#' + Integer.toString(item.getId())),
+                    new Span(item.getName()));
+            final var outer_container = new HorizontalLayout(
+                    new Span(Character.toString(item.getType().name().charAt(0))),
+                    inner_container);
+            inner_container.getChildren().forEach(c -> (((HasStyle) c)).getStyle().set("all", "initial"));
+            super.add(outer_container);
         }
 
     }
 
     private VerticalLayout outer = new VerticalLayout();
     private HorizontalLayout inner = new HorizontalLayout();
-    private VerticalScrollLayout item_list = new VerticalScrollLayout();
+    private Tabs item_list = new Tabs();
     private ArrayList<ListItem> items = new ArrayList<>();
     private Div detailed_pane = new Div();
 
@@ -40,25 +51,36 @@ public class TrackallView extends View {
         detailed_pane.getStyle().set("width", "100%");
         detailed_pane.getStyle().set("background-color", "#000080");
         detailed_pane.add(new Span("It all works, except not #2351"));
-        item_list.setWidth("375px");
+        item_list.setOrientation(Tabs.Orientation.VERTICAL);
         item_list.setHeightFull();
         item_list.getStyle().set("background-color", "green");
 
-        inner.add(item_list, detailed_pane);
+        final var scroller =  new VerticalScrollLayout(item_list);
+        scroller.setWidth("375px");
+        inner.add(scroller, detailed_pane);
         inner.getStyle().set("width", "100%");
         inner.setHeightFull();
         outer.add(inner);
         outer.setHeightFull();
         add(outer);
 
-        final var el = new ListItem(new Span("Hello guys 'n' gals"));
+        items.add(new ListItem(new TrackItem(3, "Bob is broken", "Bob doesn't work since the update", 3,2, TrackItem.Type.BUG, true, TrackItem.Status.OPEN, TrackItem.Resolution.UNRESOLVED, LocalDateTime.now(), LocalDateTime.now())));
+        items.add(new ListItem(new TrackItem(45, "Change color", "Green is better than yellow", 3,1, TrackItem.Type.FEATURE_REQUEST, true, TrackItem.Status.IN_PROGRESS, TrackItem.Resolution.FIXED, LocalDateTime.now(), LocalDateTime.now())));
 
-        items.add(el);
+        item_list.addSelectedChangeListener(e -> update_details(((ListItem) e.getSelectedTab()).item));
+
         sync_list();
+        update_details(items.get(0).item);
     }
 
     private void sync_list() {
         item_list.removeAll();
         item_list.add(items.toArray(new ListItem[]{}));
+    }
+
+    private void update_details(TrackItem ti) {
+        if(ti == null){
+            // TODO: dflt
+        }
     }
 }
