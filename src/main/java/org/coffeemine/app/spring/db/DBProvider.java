9de949514540789be162e1cf37629cc80fcaeed1
crz11111 <crz11111@outlook.com>
2019-12-15 18:20:00 +0100

    Set up database for tasks and sprints in calendar
    Add the methods use for fetch the tasks and sprints data

--- src/main/java/org/coffeemine/app/spring/db/DBProvider.java
+++ src/main/java/org/coffeemine/app/spring/db/DBProvider.java
@@ -2,11 +2,14 @@ package org.coffeemine.app.spring.db;
 
 import org.coffeemine.app.spring.data.*;
 
+import java.sql.SQLTransactionRollbackException;
 import java.util.stream.Stream;
 
 public interface DBProvider {
     void importJSONProject(String json);
     Stream<Project> getProjects();
+    Stream<ISprint> getSprints();
+    Stream<ITask> getTasks();
     Stream<ISprint> getSprints4Project(Project project);
     Stream<ITask> getTasks4Project(Project project);
     Stream<ITask> getTasks4Sprint(ISprint sprint);
@@ -15,6 +18,10 @@ public interface DBProvider {
     Stream<User> getUsers();
     User getUser(int id);
     void addUser(User user);
+    Sprint getSprint(int id);
+    void addSprint(Sprint sprint);
+    Task getTask(int id);
+    void addTask(Task task);
     Integer account_id(String name, String hashpass);
 
     Integer idFor(Class<?> c);
