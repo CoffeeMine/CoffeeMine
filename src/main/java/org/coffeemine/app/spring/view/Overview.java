0940369c8d625b6a72de182e37461a7313386dfe
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-12-15 14:57:11 +0100

    Arguably better design for overview :^)

--- src/main/java/org/coffeemine/app/spring/view/Overview.java
+++ src/main/java/org/coffeemine/app/spring/view/Overview.java
@@ -1,9 +1,6 @@
 package org.coffeemine.app.spring.view;
 
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -11,7 +8,6 @@ import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.material.Material;
 
-import org.coffeemine.app.spring.components.Taskbox;
 import org.coffeemine.app.spring.annonations.NavbarRoutable;
 
 @Route(value = "")
@@ -24,35 +20,6 @@ import org.coffeemine.app.spring.annonations.NavbarRoutable;
 public class Overview extends View {
 
     public Overview() {
-        super();
-        add(new H1("Tasks for sprint #3"));
-
-        Div layout = new Div();
-        layout.getStyle().set("overflow-x", "auto");
-        this.add(layout);
-
-        HorizontalLayout taskLayout = new HorizontalLayout();
-        taskLayout.setSizeUndefined();
-        layout.add(taskLayout);
-
-        Taskbox one = new Taskbox("Task one", "Lorem ipsum");
-        taskLayout.add(one);
-
-        Taskbox two = new Taskbox("Task two or maybe ten who knows", "Lorem ipsum and a lot of other things as well");
-        taskLayout.add(two);
-
-        Taskbox three = new Taskbox("Task three", "Lorem ipsum");
-        taskLayout.add(three);
-
-        Taskbox four = new Taskbox("Task four", "Lorem ipsum");
-        taskLayout.add(four);
-
-        Taskbox five = new Taskbox("Task five", "Lorem ipsum");
-        taskLayout.add(five);
-
-        Taskbox six = new Taskbox("Task six", "Lorem ipsum");
-        taskLayout.add(six);
-
     }
 
 }
