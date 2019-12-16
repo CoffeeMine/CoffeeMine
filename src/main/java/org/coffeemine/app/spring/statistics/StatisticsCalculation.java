7284f1f17029b825c10657d20376c960d964884d
BhavyaShukla <bhavyashukla2002@gmail.com>
2019-12-16 19:27:20 +0100

    Created the logic for the calculations
    Instead of random attributes, it is now using the database to do the calculations

--- src/main/java/org/coffeemine/app/spring/statistics/StatisticsCalculation.java
+++ src/main/java/org/coffeemine/app/spring/statistics/StatisticsCalculation.java
@@ -1,56 +1,102 @@
 package org.coffeemine.app.spring.statistics;
 
-import java.util.Date;
+import org.coffeemine.app.spring.data.*;
+import org.coffeemine.app.spring.db.NitriteDBProvider;
+
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StatisticsCalculation {
 
-    /*private Date plannedStart;
-    private Date plannedEnd;
-    private Date actualEnd;
-    private int salary;
-    private int hoursPerDay;
-    private int numberOfPeople;
-    private boolean completed;*/
+    private NitriteDBProvider dbProvider;
 
-    public StatisticsCalculation() {
+    public StatisticsCalculation(){
 
     }
 
-    public static int plannedValue(Date plannedStart, Date plannedEnd, int hours, int salary, int numberOfPeople) {
-        int totalDays = (int) Math.round((plannedStart.getTime() - plannedEnd.getTime()) / (1000 * 60 * 60 * 24));
-        int plannedValue = totalDays * hours * salary * numberOfPeople;
-        return plannedValue;
+    private int getSalary (ITask task){
+        List<Integer> users = task.getAssignees();
+        ArrayList<Integer> salary = new ArrayList<Integer>();
+
+        for (int i = 0; i < users.size(); i++){
+            int userSalary = (int) dbProvider.getUser(users.get(i)).getHourlySalary();
+            salary.add(userSalary);
+        }
+
+        return salary.stream().mapToInt(Integer::intValue).sum();
     }
 
-    public static int actualValue(Date plannedStart, Date actualEnd, int hours, int salary, int numberOfPeople) {
-        int totalDays = (int) Math.round((plannedStart.getTime() - actualEnd.getTime()) / (1000 * 60 * 60 * 24));
-        int actualValue = totalDays * hours * salary * numberOfPeople;
+    public int actualValue(ITask task) {
+        int actualValue = 0;
+        if (task.getCompleted()) {
+            List<Integer> days = dbProvider.getFragments4Task(task).map(Fragment::getDays).collect(Collectors.toList());
+            int sumOfDays = days.stream().mapToInt(Integer::intValue).sum();
+
+            actualValue = sumOfDays * getSalary(task) * task.getHours();
+        }
         return actualValue;
     }
 
-    //Other methods that need tasks and sprints to be implemented
-    /*public static int earnedValue (boolean completed){
-        int sum = 0;
-        if (completed){
-            sum = sum + task.plannedValue;
+    public int actualValueSprint (ISprint sprint){
+        List<ITask> tasks = dbProvider.getTasks4Sprint(sprint).collect(Collectors.toList());
+        int actualValueSprint = 0;
+
+        for (int i = 0; i< tasks.size(); i++){
+            actualValueSprint = actualValueSprint + actualValue(tasks.get(i));
         }
-        return sum;
-    }*/
 
-    /*public static int costVariance(){
-        return this.earnedValue - this.actualValue;
-    }*/
+        return actualValueSprint;
+    }
 
-    /*public static int scheduleVariance(){
-        return this.earnedValue - this.plannedValue;
-    }*/
+    public int plannedValue(ISprint sprint, ITask task) {
 
-    /*public static double costPerformanceIndex(){
-        return (this.earnedValue / this.actualValue) * 100;
-    }*/
+        int totalDays = (int) ChronoUnit.DAYS.between(sprint.getStart(), sprint.getEnd());
 
-    /*public static double schedulePerformanceIndex(){
-        return (this.earnedValue / this.plannedValue) * 100;
-    }*/
+        return totalDays * task.getHours() * getSalary(task);
+    }
+
+    public int plannedValueSprint(ISprint sprint){
+
+        List<ITask> tasks = dbProvider.getTasks4Sprint(sprint).collect(Collectors.toList());
+        int plannedValueSprint = 0;
+
+        for (int i = 0; i < tasks.size(); i++){
+            plannedValueSprint = plannedValueSprint + plannedValue(sprint, tasks.get(i));
+        }
+        return plannedValueSprint;
+    }
+
+    //Other methods that need tasks and sprints to be implemented
+    public int earnedValue (ISprint sprint){
+
+        List<ITask> tasks = dbProvider.getTasks4Sprint(sprint).collect(Collectors.toList());
+        int earnedValue = 0;
+
+        for (int i = 0; i < tasks.size(); i++){
+            if (tasks.get(i).getCompleted()){
+                earnedValue = earnedValue + plannedValue(sprint, tasks.get(i));
+            }
+        }
+       return earnedValue;
+    }
+
+    public int costVariance(ISprint sprint){
+        return earnedValue(sprint) - actualValueSprint(sprint);
+    }
+
+    public int scheduleVariance(ISprint sprint){
+        //return this.earnedValue - this.plannedValue;
+        return earnedValue(sprint) - plannedValueSprint(sprint);
+    }
+
+    public double costPerformanceIndex(ISprint sprint){
+        return (earnedValue(sprint) / actualValueSprint(sprint)) * 100;
+    }
+
+    public double schedulePerformanceIndex(ISprint sprint){
+        return (earnedValue(sprint) / plannedValueSprint(sprint)) * 100;
+    }
 
 }
