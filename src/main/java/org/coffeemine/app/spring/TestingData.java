2fded447ad5dd0b4348f7a6c3d762114f9e28a58
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-12-16 17:52:02 +0100

    add crude testing data to db

---
+++ src/main/java/org/coffeemine/app/spring/TestingData.java
@@ -0,0 +1,48 @@
+package org.coffeemine.app.spring;
+
+import org.coffeemine.app.spring.db.NitriteDBProvider;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.coffeemine.app.spring.data.Project;
+import org.coffeemine.app.spring.data.Sprint;
+import org.coffeemine.app.spring.data.Task;
+import org.coffeemine.app.spring.data.User;
+
+public class TestingData {
+
+    private TestingData() {
+    }
+
+    static void load() {
+        var db = NitriteDBProvider.getInstance();
+        var defaultUser = new User(0, "Admoon", User.Status.ADMIN, -1.0f, "admoon", "Foobar");
+        var defaultProject = new Project("CoffeeMine", db.idFor(Project.class));
+        
+        db.addUser(defaultUser);
+        db.addProject(defaultProject);
+
+        var users = new ArrayList<Integer>();
+        users.add(0);
+        var task = new Task(db.idFor(Task.class), "Hello", "Task1", users, new ArrayList<Integer>(), new ArrayList<String>());
+        var task1 = new Task(db.idFor(Task.class), "Hello1", "Task2", users, new ArrayList<Integer>(), new ArrayList<String>());
+        var task2 = new Task(db.idFor(Task.class), "Hello2", "Task3", users, new ArrayList<Integer>(), new ArrayList<String>());
+        var task3 = new Task(db.idFor(Task.class), "Hello3", "Task4", users, new ArrayList<Integer>(), new ArrayList<String>());
+        db.addTask(task);
+        db.addTask(task1);
+        db.addTask(task2);
+        db.addTask(task3);
+
+        var tasks = new ArrayList<Integer>();
+        tasks.add(task.getId());
+        tasks.add(task1.getId());
+        tasks.add(task2.getId());
+        tasks.add(task3.getId());
+
+        var testsprint = new Sprint(db.idFor(Sprint.class), LocalDate.now(), LocalDate.now(), -1, tasks);
+            db.addSprint(testsprint);
+
+        defaultProject.addSprint(testsprint.getId());
+
+    }
+}
