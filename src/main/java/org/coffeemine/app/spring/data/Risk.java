e82a66a43857e9b35dfd88596c6457e36e931f10
BhavyaShukla <bhavyashukla2002@gmail.com>
2020-01-09 14:46:05 +0100

    Added id attribute to risk

--- src/main/java/org/coffeemine/app/spring/data/Risk.java
+++ src/main/java/org/coffeemine/app/spring/data/Risk.java
@@ -1,17 +1,29 @@
 package org.coffeemine.app.spring.data;
 
-public class Risk {
+import java.io.Serializable;
 
+public class Risk implements Serializable {
+
+    private int id;
     private String description;
     private String type;
     private String impact;
     private String likelihood;
 
-    public Risk(String description, String type, String impact, String likelihood) {
+    public Risk(int id, String description, String type, String impact, String likelihood) {
+        this.id = id;
         this.description = description;
         this.type = type;
         this.impact = impact;
         this.likelihood = likelihood;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getDescription() {
