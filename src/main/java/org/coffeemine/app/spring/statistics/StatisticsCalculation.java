a942aafb8156049dc5ae9fe8025355d0742c5797
BhavyaShukla <bhavyashukla2002@gmail.com>
2020-01-09 17:46:30 +0100

    Minor changes to calculations

--- src/main/java/org/coffeemine/app/spring/statistics/StatisticsCalculation.java
+++ src/main/java/org/coffeemine/app/spring/statistics/StatisticsCalculation.java
@@ -1,9 +1,6 @@
 package org.coffeemine.app.spring.statistics;
 
-import org.coffeemine.app.spring.data.Fragment;
-import org.coffeemine.app.spring.data.ISprint;
-import org.coffeemine.app.spring.data.ITask;
-import org.coffeemine.app.spring.data.User;
+import org.coffeemine.app.spring.data.*;
 import org.coffeemine.app.spring.db.DBProvider;
 import org.coffeemine.app.spring.db.NitriteDBProvider;
 
@@ -27,22 +24,19 @@ public class StatisticsCalculation {
     }
 
     public float actualValue(ITask task) {
-        float actual_value = 0;
-        if (task.isCompleted()) {
-            final var sum_of_days = dbProvider()
-                    .getFragments4Task(task)
-                    .map(Fragment::getHours)
-                    .mapToInt(Integer::intValue)
-                    .sum();
+        final var sum_of_days = dbProvider()
+                .getFragments4Task(task)
+                .map(Fragment::getHours)
+                .mapToInt(Integer::intValue)
+                .sum();
 
-            actual_value = sum_of_days * getSalary(task) * task.getHours();
-        }
-        return actual_value;
+        return sum_of_days * getSalary(task) * task.getHours();
     }
 
     public float actualValueSprint(ISprint sprint) {
         return dbProvider()
                 .getTasks4Sprint(sprint)
+                .filter(ITask::isCompleted)
                 .reduce(0.0f, (f, t) -> f + actualValue(t), Float::sum);
     }
 
@@ -59,10 +53,10 @@ public class StatisticsCalculation {
 
     }
 
-    //Other methods that need tasks and sprints to be implemented
     public float earnedValue(ISprint sprint) {
         return dbProvider()
                 .getTasks4Sprint(sprint)
+                .filter(ITask::isCompleted)
                 .reduce(0.0f, (f, t) -> f + plannedValue(sprint, t), Float::sum);
     }
 
@@ -75,11 +69,19 @@ public class StatisticsCalculation {
     }
 
     public double costPerformanceIndex(ISprint sprint){
-        return (earnedValue(sprint) / actualValueSprint(sprint)) * 100;
+        if (actualValueSprint(sprint) != 0) {
+            return (earnedValue(sprint) / actualValueSprint(sprint)) * 100;
+        } else {
+            return 0;
+        }
     }
 
     public double schedulePerformanceIndex(ISprint sprint){
-        return (earnedValue(sprint) / plannedValueSprint(sprint)) * 100;
+        if (plannedValueSprint(sprint) != 0) {
+            return (earnedValue(sprint) / plannedValueSprint(sprint)) * 100;
+        } else {
+            return 0;
+        }
     }
 
 }
